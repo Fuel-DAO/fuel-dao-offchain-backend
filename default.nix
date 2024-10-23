75bb7c418f6ae85070643656122e8f3e84e22429
veer-pratap-fueldao <veer@fueldao.io>
2024-10-23 19:15:30 +0530

    build check update

--- default.nix
+++ default.nix
@@ -1,62 +1,90 @@
-{}:
-let
-  # The specific revision of nixpkgs we want to use
-  rev = "1c3a28d84f970e7774af04372ade06399add182e";
+name: Build and Check linting
 
-  # Fetch the Nixpkgs repository
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+on:
+  workflow_call:
+    inputs:
+      publish-artifact:
+        default: false
+        required: false
+        type: boolean
+  workflow_dispatch:
+  pull_request:
+    branches:
+      - main
 
-  # Import Nixpkgs
-  pkgs = import nixpkgs { };
+env:
+  CARGO_TERM_COLOR: always
 
-  # Fetch and import the DFX environment for Internet Computer development
-  dfx-env = import (builtins.fetchTarball "https://github.com/ninegua/ic-nix/releases/download/20240610/dfx-env.tar.gz") {
-    version = "20240610";
-    inherit pkgs;
-  };
-in
-# Override the attributes of the DFX environment
-dfx-env.overrideAttrs (old: {
-  # Adding native build inputs (tools and libraries we want available)
-  nativeBuildInputs = with pkgs; old.nativeBuildInputs ++
-    [
-      rustup              # For managing Rust toolchains
-      pkg-config          # For managing build configurations
-      openssl.dev         # OpenSSL development libraries
-      protobuf            # For working with Protocol Buffers
-      cmake               # Build system
-      cachix              # Caching for build artifacts
-      killall             # Unix utility for killing processes
-      jq                  # Command-line JSON processor
-      coreutils           # Basic GNU utilities (ls, cat, etc.)
-      bc                  # Command-line calculator
-      python3Full         # Full Python 3 environment
-      libiconv            # Text conversion library
-      wget                # Tool to download files from the web
-      nodejs              # Node.js runtime (includes npm)
-      trunk               # Trunk for managing front-end assets
-      musl                # musl for cross-compiling
-    ] ++ (if pkgs.stdenv.isDarwin then [
-      darwin.apple_sdk.frameworks.Foundation
-      pkgs.darwin.libiconv
-    ] else []);
+jobs:
+  build:
+    name: Build Check
+    runs-on: ubuntu-latest
 
-  # Shell hooks (executed when the shell starts)
-  shellHook = ''
-      # Add the wasm32 target to Rust
-      rustup target add wasm32-unknown-unknown
-      rustup target add x86_64-unknown-linux-musl
-      # Install candid-extractor (needed for IC projects)
-      cargo install --root $out --force candid-extractor
-      ln -s $out/bin/candid-extractor $out/bin/candid-extractor
+    env:
+      BACKEND: ${{ secrets.BACKEND }}
+      RUST_LOG: ${{ secrets.RUST_LOG }}
+      SERVER_PORT: ${{ secrets.SERVER_PORT }}
+      EMAIL_CLIENT_ID: ${{ secrets.EMAIL_CLIENT_ID }}
+      EMAIL_CLIENT_SECRET: ${{ secrets.EMAIL_CLIENT_SECRET }}
+      EMAIL_ACCESS_TOKEN: ${{ secrets.EMAIL_ACCESS_TOKEN }}
+      EMAIL_REFRESH_TOKEN: ${{ secrets.EMAIL_REFRESH_TOKEN }}
 
-      # Add Node.js and npm binaries to PATH (ensuring they are available globally)
-      export PATH="$out/bin:$PATH"
+    steps:
+      - name: Checkout Code
+        uses: actions/checkout@v3
 
-      # Print installed versions of node, npm, and trunk to verify installation
-      echo "Node.js version: $(node -v)"
-      echo "npm version: $(npm -v)"
-      echo "Trunk version: $(trunk -V)"
-    '';
-})
+      - name: Cache install Nix packages
+        uses: rikhuijzer/cache-install@v1.1.4
+        with:
+          key: nix-${{ hashFiles('default.nix') }}
+
+      - name: Cache Rust dependencies, build output, and DFX build cache
+        uses: actions/cache@v3
+        with:
+          path: |
+            ~/.cargo/bin/
+            ~/.cargo/registry/index/
+            ~/.cargo/registry/cache/
+            ~/.cargo/git/db/
+            target/
+          key: rust-test-${{ hashFiles('**/Cargo.lock') }}
+
+      - name: Install prerequisites
+        run: |
+          sudo apt-get update
+          sudo apt-get install -y protobuf-compiler musl-tools libssl-dev
+
+      - name: Rust Setup
+        uses: dtolnay/rust-toolchain@master
+        with:
+          toolchain: "stable"
+          targets: "x86_64-unknown-linux-musl"
+
+      - name: Build setup
+        run: |
+          rustup target add x86_64-unknown-linux-musl
+          rustup component add rustfmt
+          rustup component add clippy
+
+      - name: Set up pkg-config for cross-compilation
+        run: |
+          echo 'export PKG_CONFIG="/usr/bin/x86_64-linux-musl-gcc -pkg-config"' >> $GITHUB_ENV
+          echo 'export PKG_CONFIG_PATH="/usr/lib/x86_64-linux-musl/pkgconfig"' >> $GITHUB_ENV
+
+      - name: Build
+        run: cargo build --release --target x86_64-unknown-linux-musl --verbose
+        env:
+          TARGET_CC: x86_64-linux-musl-gcc
+          OPENSSL_DIR: /usr/include  # Adjust if necessary
+
+      - run: touch .empty
+
+      - name: Archive production artifacts
+        uses: actions/upload-artifact@v4
+        if: ${{ inputs.publish-artifact }}
+        with:
+          name: build-musl
+          path: |
+            target/x86_64-unknown-linux-musl/release/fuel-dao-off-chain-backend
+            .empty
 
