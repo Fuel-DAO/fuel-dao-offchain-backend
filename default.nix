b4e2cf2b88614b35c149877193233a45d6a2fbf8
veer-pratap-fueldao <veer@fueldao.io>
2024-10-24 10:08:04 +0530

    nix setup update

--- default.nix
+++ default.nix
@@ -2,13 +2,22 @@
 let
   # Specific revision of nixpkgs
   rev = "1c3a28d84f970e7774af04372ade06399add182e";
-
   # Fetch the Nixpkgs repository
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-
-  # Import Nixpkgs
-  pkgs = import nixpkgs { };
-
+  # Import Nixpkgs with overlay for musl
+  pkgs = import nixpkgs {
+    overlays = [
+      (self: super: {
+        muslPackages = import nixpkgs {
+          localSystem = "x86_64-linux";
+          crossSystem = {
+            config = "x86_64-unknown-linux-musl";
+            libc = "musl";
+          };
+        };
+      })
+    ];
+  };
   # Fetch and import the DFX environment for Internet Computer development
   dfx-env = import (builtins.fetchTarball "https://github.com/ninegua/ic-nix/releases/download/20240610/dfx-env.tar.gz") {
     version = "20240610";
@@ -20,26 +29,31 @@ dfx-env.overrideAttrs (old: {
   # Adding native build inputs (tools and libraries we want available)
   nativeBuildInputs = with pkgs; old.nativeBuildInputs ++
     [
-      rustup              # For managing Rust toolchains
+      rustup
       binaryen
-      pkg-config          # For managing build configurations
+      pkg-config
       openssl
-      openssl.dev         # OpenSSL development libraries
-      protobuf            # For working with Protocol Buffers
+      openssl.dev
+      protobuf
       protobuf_21
-      cmake               # Build system
-      cachix              # Caching for build artifacts
-      killall             # Unix utility for killing processes
-      jq                  # Command-line JSON processor
-      coreutils           # Basic GNU utilities (ls, cat, etc.)
-      bc                  # Command-line calculator
-      python3Full         # Full Python 3 environment
-      libiconv            # Text conversion library
-      wget                # Tool to download files from the web
-      nodejs              # Node.js runtime (includes npm)
-      trunk               # Trunk for managing front-end assets
-      musl                # musl for cross-compiling
-      gcc                 # C Compiler
+      cmake
+      cachix
+      killall
+      jq
+      coreutils
+      bc
+      python3Full
+      libiconv
+      wget
+      nodejs
+      trunk
+      musl
+      gcc
+      muslPackages.stdenv.cc  # Add musl-specific compiler
+      file                    # For file type identification
+      gnumake                 # Make build system
+      binutils               # Basic binary utilities
+      binutils.bintools      # Additional binary tools
     ] ++ (if pkgs.stdenv.isDarwin then [
       darwin.apple_sdk.frameworks.Foundation
       pkgs.darwin.libiconv
@@ -47,29 +61,59 @@ dfx-env.overrideAttrs (old: {
 
   # Build dependencies for cross-compilation
   buildInputs = with pkgs; old.buildInputs ++ [
-    openssl.dev  # OpenSSL for cross-compilation
+    openssl.dev
+    muslPackages.stdenv.cc.libc  # Add musl libc
+    zlib.dev
+    zlib.static                  # Static zlib for musl builds
   ];
+
+  # Environment variables for cross-compilation
+  NIX_LDFLAGS = "-L${pkgs.openssl.out}/lib -L${pkgs.zlib.static}/lib";
 
   # Shell hooks (executed when the shell starts)
   shellHook = ''
-      # Add the musl target for Rust
-      rustup target add wasm32-unknown-unknown
-      rustup target add x86_64-unknown-linux-musl
-      rustup component add rustfmt
-      rustup component add clippy
+    # Setup cross-compilation environment
+    export CC_x86_64_unknown_linux_musl="${pkgs.muslPackages.stdenv.cc}/bin/x86_64-unknown-linux-musl-gcc"
+    export CXX_x86_64_unknown_linux_musl="${pkgs.muslPackages.stdenv.cc}/bin/x86_64-unknown-linux-musl-g++"
+    export AR_x86_64_unknown_linux_musl="${pkgs.muslPackages.stdenv.cc}/bin/x86_64-unknown-linux-musl-ar"
+    export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER="${pkgs.muslPackages.stdenv.cc}/bin/x86_64-unknown-linux-musl-gcc"
 
-      # Install candid-extractor
-      cargo install --root $out --force candid-extractor
-      ln -s $out/bin/candid-extractor $out/bin/candid-extractor
+    # OpenSSL configuration for cross-compilation
+    export OPENSSL_DIR="${pkgs.openssl.dev}"
+    export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
+    export OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include"
+    export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
+    export PKG_CONFIG_ALLOW_CROSS=1
 
+    # Add the musl target for Rust
+    rustup target add wasm32-unknown-unknown
+    rustup target add x86_64-unknown-linux-musl
+    rustup component add rustfmt
+    rustup component add clippy
 
-      # Add Node.js and npm binaries to PATH
-      export PATH="$out/bin:$PATH"
+    # Create cargo config for cross-compilation
+    mkdir -p ~/.cargo
+    cat > ~/.cargo/config.toml << EOF
+    [target.x86_64-unknown-linux-musl]
+    linker = "x86_64-unknown-linux-musl-gcc"
+    rustflags = [
+      "-C", "target-feature=+crt-static",
+      "-C", "link-arg=-static"
+    ]
+    EOF
 
-      # Print versions
-      echo "Node.js version: $(node -v)"
-      echo "npm version: $(npm -v)"
-      echo "Trunk version: $(trunk -V)"
-    '';
+    # Install candid-extractor
+    cargo install --root $out --force candid-extractor
+    ln -sf $out/bin/candid-extractor $out/bin/candid-extractor
+
+    # Add Node.js and npm binaries to PATH
+    export PATH="$out/bin:$PATH"
+
+    # Print versions
+    echo "Node.js version: $(node -v)"
+    echo "npm version: $(npm -v)"
+    echo "Trunk version: $(trunk -V)"
+    echo "GCC version: $(gcc --version | head -n1)"
+    echo "Musl CC version: ${pkgs.muslPackages.stdenv.cc}/bin/x86_64-unknown-linux-musl-gcc --version | head -n1)"
+  '';
 })
-
