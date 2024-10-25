ee33aade883333c53636032abdea9026433ff416
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2024-10-25 13:56:32 +0530

    Added log:

--- src/lib/inbound/http.rs
+++ src/lib/inbound/http.rs
@@ -57,6 +57,8 @@ impl HttpServer {
             .layer(trace_layer)
             .with_state(state);
 
+        tracing::info!("Config on {:?}", config);
+
         let addr = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], config.port.parse::<u16>()?));
         // let addr =format!("0.0.0.0:{}", config.port);
 
